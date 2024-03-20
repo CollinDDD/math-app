c2e9515f8b30704bd4c3f40a0a27b7f35d7757a5
Collin <collindewalt@gmail.com>
2024-03-20 02:08:14 -0400

    completed backend
    added functions to create new users, retrieve  data for specific users, and updates for users' high scores

--- server/users/users.controller.js
+++ server/users/users.controller.js
@@ -2,9 +2,6 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const usersService = require("./users.service");
 
 
-async function list(req, res) {
-    res.json({ data: await usersService.list() });
-}
 
 async function create(req, res) {
     const newUser = req.body;
@@ -12,9 +9,31 @@ async function create(req, res) {
     res.status(201).json({message: `User: ${newUser} created successfully`});
 }
 
+async function readUserData(req, res) {
+    const {userId} = req.params;
+    res.json({ data: await usersService.readUserData(userId) });
+}
 
+async function updateHighScore(req, res, next) {
+    const userId = req.params.userId;
+    const { user_highscore } = req.body;
+
+    const user = await usersService.readUserData(userId);
+    const currentHighScore = user.user_highscore;
+
+    let message;
+    if (user_highscore > currentHighScore) {
+        await usersService.updateHighScore(userId, user_highscore);
+        message = `Your new high score is ${user_highscore}!`;
+    } else {
+        message = `Your high score is ${currentHighScore}`;
+    }
+
+    res.status(200).json({ data: { user_highscore }, message });
+}
 
 module.exports = {
-    list: asyncErrorBoundary(list),
-    create: asyncErrorBoundary(create)
+    read: asyncErrorBoundary(readUserData),
+    create: asyncErrorBoundary(create),
+    update: asyncErrorBoundary(updateHighScore),
 };
--- server/users/users.service.js
+++ server/users/users.service.js
@@ -1,16 +1,25 @@
 const knex = require("../db/connection");
 
-
-function list() {
-    return knex("users")
-        .select("*");
-}
-
 function create(newUser) {
     return knex("users")
         .insert(newUser);
 }
+
+function readUserData(userId) {
+    return knex("users")
+        .select("*")
+        .where({user_id: userId})
+        .first();
+}
+
+function updateHighScore(userId, newHighScore) {
+    return knex("users")
+        .where({ user_id: userId})
+        .update({user_highscore: newHighScore});
+}
+
 module.exports = {
-    list,
-    create
+    readUserData,
+    create,
+    updateHighScore,
 };
